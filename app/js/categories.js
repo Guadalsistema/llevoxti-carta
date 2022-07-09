bbe556205d7d57f57e810e8ba99af75effd3bc8e
Juan Vázquez Moreno <vmjuan90@gmail.com>
2022-07-09 17:22:11 +0200

    added category-card

--- app/js/categories.js
+++ app/js/categories.js
@@ -1,14 +1,16 @@
-import { config } from 'config';
+import { config } from './config.js';
 
 function getCategories() {
-	var url = config["url"];
+	var url = config["url"] + "/menu/category";
 
 	fetch(url, {
 	  method: 'GET',
 	  headers:{
-	    'Content-Type': 'application/json'
-	  }
+	    'Content-Type': 'application/json',
+	  },
 	}).then(res => res.json())
 	.catch(error => console.error('Error:', error))
 	.then(response => console.log('Success:', response));
 }
+
+getCategories();
