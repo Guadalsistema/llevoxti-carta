19a7040d07a90536b5d53cc5d6bec04de493789a
Johnny VM <vmjuan90@gmail.com>
2022-07-17 01:48:18 +0200

    add product-card

--- app/js/product-element.js
+++ app/js/product-element.js
@@ -1,9 +1,41 @@
 class ProductCard extends HTMLElement {
 	constructor(){
 		super();
+		let shadow = this.attachShadow({mode: 'open'});
+		let productbox = document.createElement('div');
+		productbox.setAttribute('class', 'product__box');
 
-		// write element functionality in here
+			let productName = document.createElement('div');
+			productName.setAttribute('class', 'product__name');
+			productbox.appendChild(productName);
+			let hdos = document.createElement('h2');
+			productName.appendChild(hdos);
 
+			let productAmount = document.createElement('div');
+			productAmount.setAttribute('class', 'product__amount');
+			productbox.appendChild(productAmount);
+				let productButton = document.createElement('div');
+				productButton.setAttribute('class', 'product__button');
+				productAmount.appendChild(productButton);
+				productButton.appendChild(document.createElement('div'));
+				productButton.appendChild(document.createElement('div'));
+				let productNumber = document.createElement('div');
+				productNumber.setAttribute('class', 'product__number');
+				productAmount.appendChild(productNumber);
+					let p = document.createElement('p');
+					p.textContent = 1;
+					productNumber.appendChild(p);
+				let productButton2 = document.createElement('div');
+				productButton2.setAttribute('class', 'product__button');
+				productAmount.appendChild(productButton2);
+				productButton2.appendChild(document.createElement('div'));
+			let productPrize = document.createElement('div');
+			productPrize.setAttribute('class', 'product__prize');
+			productbox.appendChild(productPrize);
+				let htres = document.createElement('h3');
+				productPrize.appendChild(htres);
+
+		shadow.appendChild(productbox);
 	}
 }
 
