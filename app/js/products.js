216940c77e88464a8c6d41394f38d015048b6540
Johnny VM <vmjuan90@gmail.com>
2022-07-16 21:31:03 +0200

    WIP - First approach (#7)
    * Update products.js

--- app/js/products.js
+++ app/js/products.js
@@ -1,28 +1,46 @@
 import { config } from './config.js';
-import { ProductCard } from './product-element.js'
 
-function displayCategories(products) {
-	let placeholder = document.getElementById("product-list");
-	let pTemplate = document.getElementById('product-row-list');
+function displayProducts(products) {
+	let pTemplate = document.getElementById("product-row-tmpl");
+	let placeholder = document.querySelector('div.product');
 	for (const product of products) {
-		let card = pTemplate.content.querySelector('product-card');
-		card.setAttribute("product-id", product.id);
-		card.setAttribute("category-id", product.pos_categ_id);
-		card.setAttribute("name", product.name);
+		let container = pTemplate.content.querySelector('.product__box');
+		let name = pTemplate.content.querySelector('h2');
+		let price = pTemplate.content.querySelector('h3');
+		name.textContent = product.name;
+		price.textContent = product.lst_price + '€';
+		container.setAttribute('product-id', product.id);
+		container.setAttribute('category-id', product.pos_categ_id);
+		var clone = document.importNode(pTemplate.content, true);
+		placeholder.appendChild(clone);
+	}
+}
+
+function displayCategories(categories) {
+	let pTemplate = document.getElementById("li-category-tmpl");
+	let placeholder = document.querySelector('ul');
+	for (const category of categories) {
+		let container = pTemplate.content.querySelector('.menu__categories-item');
+		container.textContent = category.name;
+		container.setAttribute('category-id', category.id);
 		var clone = document.importNode(pTemplate.content, true);
 		placeholder.appendChild(clone);
 	}
 }
 
 function workProducts() {
-	var url = config["url"] + "/menu";
-	fetch(url, {
+	var url_products = config["url"] + "/menu";
+	fetch(url_products, {
 	  method: 'GET',
 	}).then(res => res.json())
-	.then(products => displayCategories(products))
+	.then(products => displayProducts(products))
+	.catch(error => console.error('Error:', error));
+	var url_categories = config["url"] + "/menu/category";
+	fetch(url_categories, {
+	  method: 'GET',
+	}).then(res => res.json())
+	.then(categories => displayCategories(categories))
 	.catch(error => console.error('Error:', error));
 }
-
-customElements.define('product-card', ProductCard);
 
 workProducts();
