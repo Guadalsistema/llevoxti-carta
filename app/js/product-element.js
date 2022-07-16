2f40e1e3d958180972ad90d11547dc1681c03dc1
Johnny VM <vmjuan90@gmail.com>
2022-07-17 00:52:19 +0200

    fix css

--- app/js/product-element.js
+++ app/js/product-element.js
@@ -1,10 +1,6 @@
 class ProductCard extends HTMLElement {
 	constructor(){
 		super();
-		let shadow = this.attachShadow({mode: 'open'});
-		const style = document.createElement('style');
-		style.textContent = ``;
-		shadow.appendChild(style);
 		// write element functionality in here
 	}
 }
