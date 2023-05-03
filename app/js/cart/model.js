b345a97e9964ab26657ff991362f6e8f538df14d
jvr63 <48151336+jvr63@users.noreply.github.com>
2023-05-03 18:19:40 +0200

    Reparacion carrito (#50)
    * Reparado ordenacion de articulos sort
    
    * Reparado ordenacion de articulos sort_1
    
    * Añadido pedido minimo
    
    * corregido error  pedido minimo
    
    * Reparado pedido minimo
    
    * Modificado css carrito y añadido boton enviar
    
    * corregir carrito
    
    * añdido notas a productos del carrito
    
    * añadir nota al order carrito
    
    * Uploaded nota y sended to web
    
    * reparado notas de productos del carrito
    
    * reparado undefined
    
    * reparado nota de articulo entrega a domicilio
    
    * reparado carritos
    
    ---------
    
    Co-authored-by: Johnny VM <vmjuan90@gmail.com>

--- app/js/cart/model.js
+++ app/js/cart/model.js
@@ -28,7 +28,6 @@ class Cart {
         //if(cart[i].note != newProduct.note) {
             //cart[i].note = newProduct.note;
         //}
-
         sessionStorage.setItem('order', JSON.stringify(cart));
         this.change_qty.emit(this.length);
         this.product_updated.emit(newProduct);
@@ -47,7 +46,6 @@ class Cart {
                     //newProduct.note = cart[i].note;
                     cart[i].note = newProduct.note
                 };
-
                 found = true;
                 break;
             }
